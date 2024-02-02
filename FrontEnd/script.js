9088f9f7bdd05750ecd58a57c490ff1e74ba3878
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-02-02 16:13:07 +0100

    suppression du contenu gallery HTML/ajout de la galerie en dynamique avec JS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,33 @@
-const fetchDataApi = () => {
-  fetch(`http://localhost:5678/api/works`)
+const projectsContainer = document.querySelector(".gallery");
+let projects = [];
+// je crée une boite pour mettre à l'intérieur les données de l'API
+
+// console.log(projectsContainer);
+
+// je récupère les données de l'API avec un fetch
+async function fetchProjects() {
+  await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
-    .then((data) => console.log(data));
-};
-fetchDataApi();
+    .then((data) => (projects = data));
+
+  console.log(projects);
+  // une fois que le fetch est appliqué, on appelle la fonction d'affichage
+  projectsDisplay();
+}
+
+// je créer ma fonction d'affichage
+function projectsDisplay() {
+  projectsContainer.innerHTML = projects
+    .map(
+      (work) => `
+      <figure class="project-card">
+        <img src="${work.imageUrl}" alt="photo ${work.title}">
+        <figcaption>${work.title}</figcaption>
+      </figure>
+  `
+    )
+    .join("");
+}
+
+// je veux que la fonction de fetch se joue dès l'ouverture de la page web
+window.addEventListener("load", fetchProjects);
