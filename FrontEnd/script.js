aaf8bc82ce55820399c74b92bbe0e04f4ac8e3d2
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-02-06 14:48:04 +0100

    création des filtres reliés au bouton correspondant

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,10 @@
 // je crée une boite pour mettre à l'intérieur les données de l'API
 const projectsContainer = document.querySelector(".gallery");
 // récupération des boutons de la barre de filtres
-const filterBtnAll = document.querySelectorAll(".btnFilter");
 const buttonObjets = document.getElementById("btn-objets");
 const buttonAppartements = document.getElementById("btn-appartements");
 const buttonHotelResto = document.getElementById("btn-hotel-resto");
 const buttonTous = document.getElementById("btn-tous");
-console.log(buttonObjets);
 
 let projects = [];
 let category = [];
@@ -16,18 +14,26 @@ async function fetchProjects() {
   await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) => (projects = data));
-
   console.log(projects);
-  // une fois que le fetch est appliqué, on appelle la fonction d'affichage
+  // une fois que le fetch est appliqué, j'appelle la fonction d'affichage
   projectsDisplay();
+}
+
+// je récupère également les données categorie
+async function fetchCategory() {
+  await fetch("http://localhost:5678/api/categories")
+    .then((res) => res.json())
+    .then((data) => (category = data));
+  console.log(category);
 }
 
 // je crée ma fonction d'affichage
 function projectsDisplay() {
   projectsContainer.innerHTML = projects
+    .filter((work) => work.category.name)
     .map(
       (work) => `
-    <figure class="project-card">
+    <figure class="project-card active" data-"${work.category.name}">
     <img src="${work.imageUrl}" alt="photo ${work.title}">
     <figcaption>${work.title}</figcaption>
     </figure>
@@ -36,30 +42,54 @@ function projectsDisplay() {
     .join("");
 }
 
-// je récupère les données categorie pour réaliser les déifférents filtres
-async function fetchCategory() {
-  await fetch("http://localhost:5678/api/categories")
-    .then((res) => res.json())
-    .then((data) => (category = data));
-  console.log(category);
-  filterDisplay();
+// je crée mes différents filtres en utilisant filter relié au click du bouton correspondant
+function projectsDisplayObjets() {
+  projectsContainer.innerHTML = projects
+    .filter((work) => work.category.name.includes("Objets"))
+    .map(
+      (work) => `
+    <figure class="project-card active" data-"${work.category.name}">
+    <img src="${work.imageUrl}" alt="photo ${work.title}">
+    <figcaption>${work.title}</figcaption>
+    </figure>
+    `
+    )
+    .join("");
 }
 
-function filterDisplay() {
-  buttonObjets.addEventListener("click", function () {
-    console.log("click");
-  });
-  buttonAppartements.addEventListener("click", function () {
-    console.log("click");
-  });
-  buttonHotelResto.addEventListener("click", function () {
-    console.log("click");
-  });
-  buttonTous.addEventListener("click", function () {
-    console.log("click");
-  });
+function projectsDisplayAppartements() {
+  projectsContainer.innerHTML = projects
+    .filter((work) => work.category.name.includes("Appartements"))
+    .map(
+      (work) => `
+    <figure class="project-card active" data-"${work.category.name}">
+    <img src="${work.imageUrl}" alt="photo ${work.title}">
+    <figcaption>${work.title}</figcaption>
+    </figure>
+    `
+    )
+    .join("");
 }
 
-// je veux que la fonction de fetch se joue dès l'ouverture de la page web
+function projectsDisplayHotelResto() {
+  projectsContainer.innerHTML = projects
+    .filter((work) => work.category.name.includes("Hotels & restaurants"))
+    .map(
+      (work) => `
+    <figure class="project-card active" data-"${work.category.name}">
+    <img src="${work.imageUrl}" alt="photo ${work.title}">
+    <figcaption>${work.title}</figcaption>
+    </figure>
+    `
+    )
+    .join("");
+}
+
+buttonTous.addEventListener("click", projectsDisplay);
+buttonObjets.addEventListener("click", projectsDisplayObjets);
+buttonAppartements.addEventListener("click", projectsDisplayAppartements);
+buttonHotelResto.addEventListener("click", projectsDisplayHotelResto);
+
+// je load les fetch dès l'ouverture de la page
 window.addEventListener("load", fetchProjects);
 window.addEventListener("load", fetchCategory);
