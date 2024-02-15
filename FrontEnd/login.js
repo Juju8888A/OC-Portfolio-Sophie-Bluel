fef744f80fceec5314460658cd477a520f639803
Juliette Jourdan <jourdanjuliette@yahoo.fr>
2024-02-15 11:57:05 +0100

    correction du code concernant la validité ou erreurs des email/mdp

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,37 +1,71 @@
-// je récupère mon bouton d'envoi du formulaire
-const validBtn = document.getElementById("btn-envoi");
+// je récupère mes éléments du formulaire
+const myForm = document.getElementById("myForm");
 const mail = document.getElementById("mail");
 const mdp = document.getElementById("mdp");
-// récupération du formulaire pour l'envoi à la fin en POST
-const myForm = document.getElementById("myForm");
-// récupération des inputs du formulaire
-let mailM = document.getElementById("mail_missing");
-let mailValid = /[a-z._-]+@[a-z._-]+\.[a-z._-]+/;
-// console.log(mailValid);
-let mdpM = document.getElementById("mdp_missing");
-// console.log(mdp);
+const validBtn = document.getElementById("btn-envoi");
+// récupération des span error
+let mailMsg = document.getElementById("mail_comment");
+let mdpMsg = document.getElementById("mdp_comment");
 
-function formValid(e) {
-  // si il n'y a rien écrit dans mon champ, je mets un message d'erreur
-  if (mail.value === "") {
-    // je n'oublie pas de bloquer le renvoi du formulaire
-    e.preventDefault;
-    mailM.textContent =
-      "Veuillez remplir ce champ avec votre adresse e-mail valide.";
-    mailM.style.color = "red";
-    // si l'adresse mail écrite ne correspond pas au RegExp, il y a un message d'erreur également
-  } else if (mailValid.test(mail.value) == false) {
-    e.preventDefault();
-    mailM.textContent = "Format incorrect";
-    mailM.style.color = "red";
-  }
-  if (mdp.value === "") {
-    e.preventDefault;
-    mdpM.textContent =
-      "Veuillez remplir ce champ avec votre mot de passe valide.";
-    mdpM.style.color = "red";
-  }
-}
+// au changement de valeurs dans l'input email, je souhaite que le mailValid s'active
+mail.addEventListener("change", function () {
+  mailValid(this);
+});
 
-// je crée un evenement click sur mon bouton de validation, au clic, j'appelle la fonction formValid pour vérifier que mes champs sont bien remplis
-validBtn.addEventListener("click", formValid);
+// au changement de valeurs dans l'input password, je souhaite que le mdpValid s'active
+mdp.addEventListener("change", function () {
+  mdpValid(this);
+});
+
+// **************************** E-MAIL*****************************************
+
+// pour vérifier l'email je vérifie son contenu, que c'est bien une adressse mail avec regExp
+const mailValid = function (inputEmail) {
+  let emailRegExp = new RegExp(`^[a-z._-]+@[a-z._-]+\.[a-z._-]+$`);
+  // je teste le regExp
+  let testEmail = emailRegExp.test(inputEmail.value);
+  console.log(testEmail);
+
+  // si mon adresse est bien écrite, résultat vert sinon résultat rouge
+  if (testEmail) {
+    mailMsg.innerHTML = "Adresse Valide";
+    mailMsg.style.color = "green";
+  } else {
+    mailMsg.innerHTML = "Veuillez entrer un e-mail valide";
+    mailMsg.style.color = "red";
+  }
+};
+
+// **************************** MOT DE PASSE *****************************************
+
+const mdpValid = function (inputPassword) {
+  // quelles sont les erreurs possibles de mot de passe? Je souhaite que le mot de passe ait au moins 1 minuscule, 1 majuscule et 1 chiffre et qu'il fasse minimum 3 caractères.
+  // j'utilise ! pour demander si il n'y a pas de majuscule, est ce que tout ça me renvoie faux?
+  let messageMdp;
+  // je mets la validation du mot de passe à faux et au fur et a mesure des tests il passera à vrai
+  let valid = false;
+
+  if (inputPassword.value.length < 3) {
+    messageMdp = "Le mot de passe doit contenir au moins 3 caractères.";
+  } else if (!/[A-Z]/.test(inputPassword.value)) {
+    messageMdp = "Le mot de passe doit contenir au moins 1 majuscule";
+  } else if (!/[a-z]/.test(inputPassword.value)) {
+    messageMdp = "Le mot de passe doit contenir au moins 1 minuscule";
+  } else if (!/[0-9]/.test(inputPassword.value)) {
+    messageMdp = "Le mot de passe doit contenir au moins 1 chiffre";
+  } else {
+    messageMdp = "Mot de passe valide";
+    valid = true;
+  }
+  // console.log(messageMdp);
+
+  // affichage résultat mot de passe valide ou non
+
+  if (valid) {
+    mdpMsg.innerHTML = "Mot de passe valide";
+    mdpMsg.style.color = "green";
+  } else {
+    mdpMsg.innerHTML = "Veuillez entrer un mot de passe valide";
+    mdpMsg.style.color = "red";
+  }
+};
